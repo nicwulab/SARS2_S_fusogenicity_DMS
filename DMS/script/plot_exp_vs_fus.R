4c04af39ad23b3af717e6a8e923f141306601ee1
Nicholas Wu <wchnicholas@Nicholass-MacBook-Pro-2.local>
2023-02-08 02:00:43 -0600

    update plot_exp_vs_fus

--- DMS/script/plot_exp_vs_fus.R
+++ DMS/script/plot_exp_vs_fus.R
@@ -16,21 +16,28 @@ library(ggforce)
 require(cowplot)
 
 x_nudging_normal <- function(label){
-  if (label == 'A899P'){return (0.1)}
+  if (label == 'V987P'){return (-1.2)}
+  if (label == 'A899P'){return (-0.2)}
   if (label == 'D994E'){return (0.5)}
   if (label == 'D994Q'){return (-1.2)}
-  if (label == 'Q1005R'){return (-1)}
+  if (label == 'Q1005R'){return (-0.7)}
   if (label == 'D950N'){return (-0.5)}
-  if (label == 'Q954H'){return (-0.5)}
+  if (label == 'Q954H'){return (-0.8)}
+  if (label == 'T1027I'){return (-0.1)}
+  if (label == 'Q1005R'){return (-1)}
   else{return (0)}
   }
 
 y_nudging_normal <- function(label){
+  if (label == 'S982A'){return (-0.1)}
+  if (label == 'V987P'){return (-0.1)}
+  if (label == 'L981F'){return (0.5)}
   if (label == 'A899P'){return (-1)}
-  if (label == 'D994E'){return (-1)}
-  if (label == 'D994Q'){return (-0.5)}
+  if (label == 'D994E'){return (-1.5)}
+  if (label == 'D994Q'){return (-1.3)}
   if (label == 'D950N'){return (0)}
-  if (label == 'Q954H'){return (-0.5)}
+  if (label == 'Q954H'){return (-0.8)}
+  if (label == 'Q1005R'){return (-0.6)}
   else{return (0)}
   }
 
@@ -64,18 +71,24 @@ plot_exp_vs_fus <- function(df, df_special, df_others, df_WT, graphname, ylab){
                      mutate(nudge_x=mapply(x_nudging_residual, mut)) %>%
                      mutate(nudge_y=mapply(y_nudging_residual, mut))
     }
-  #palette <- c('grey',qualpal(n = 5, list(h = c(0, 360), s = c(0.4, 0.6), l = c(0.5, 0.85)))$hex)
-  palette <- c('grey','#92DDAE','#7775D1','#BC4E47',qualpal(n = 5, list(h = c(0, 360), s = c(0.4, 0.6), l = c(0.5, 0.85)))$hex[1],'#DFBBE7')
   p <-  ggplot() +
-          geom_point(data=df_others, aes(x=exp_score, y=fus_score, size=log10(avg_freq), color='grey'), alpha=0.4, pch=16) +
+          geom_point(data=df_others, aes(x=exp_score, y=fus_score, size=log10(avg_freq), color='Others'), alpha=0.4, pch=16) +
           geom_point(data=df_WT, aes(x=exp_score, y=fus_score, size=log10(avg_freq), color='WT'), alpha=0.7, pch=16) +
           geom_point(data=df_special, aes(x=exp_score, y=fus_score, size=log10(avg_freq), color=grouping), alpha=0.7, pch=16) +
-          scale_color_manual(values=palette,drop=FALSE) +
+          scale_color_manual('',
+                             values=c('Others'='grey',
+                                      'S-Closed/HexaPro'='#92DDAE',
+                                      'Non-fusogenic'='#7775D1',
+                                      'Fusogenic'='#BC4E47',
+                                      'Variants'=qualpal(n = 5, list(h = c(0, 360), s = c(0.4, 0.6), l = c(0.5, 0.85)))$hex[1],
+                                      'WT'='#DFBBE7'),
+                             drop=FALSE) +
           geom_text_repel(data=df_special, aes(x=exp_score, y=fus_score,label=mut),
                           color="black", min.segment.length=0, segment.size=0.2, size=2, force=15, force_pull=1,
                           seed=5, nudge_x=label_table$nudge_x, nudge_y=label_table$nudge_y, max.overlaps = Inf) +
           geom_smooth(data=df, mapping = aes(x=exp_score, y=fus_score), method = "lm", se=FALSE, color="black", size = 0.2, lty=2) +
-          scale_size_continuous(limits = c(-4.5, -1), range = c(0.1, 2.5), breaks = c(-4, -3, -2, -1)) +
+          scale_size_continuous(expression(bold(log["10"]~'(avg freq)')),
+                                limits = c(-4.5, -1), range = c(0.1, 2.5), breaks = c(-4, -3, -2, -1)) +
           theme_cowplot(12) +
           theme(plot.title=element_text(size=textsize+2,hjust=0.5,vjust=0.5,face="bold",colour = 'black'),
                 axis.text=element_text(size=textsize,face="bold",colour = 'black'),
@@ -94,26 +107,26 @@ plot_exp_vs_fus <- function(df, df_special, df_others, df_WT, graphname, ylab){
   }
 
 coloring <- function(mut){
-  if (mut %in% c("K986P", "V987P", "A892P", "A899P", "A942P")){return ("HexaPro")}
-  else if (mut %in% c("D994E", "D994Q", "T961F", "Q1005R")){return ("This study 1")}
-  else if (mut %in% c("S943H","A944S")){return ("This study 2")}
+  if (mut %in% c("K986P", "V987P", "A892P", "A899P", "A942P")){return ("S-Closed/HexaPro")}
+  else if (mut %in% c("D994E", "D994Q", "T961F", "Q1005R")){return ("Non-fusogenic")}
+  else if (mut %in% c("S943H","A944S")){return ("Fusogenic")}
   else if (mut %in% c("WT")){return ("WT")}
-  else if (mut %in% c("D950N", "Q954H", "N969K", "L981F", "S982A", "T1027I")){return ("This study 3")}
-  else {return ('others')}
+  else if (mut %in% c("D950N", "Q954H", "N969K", "L981F", "S982A", "T1027I")){return ("Variants")}
+  else {return ('Others')}
   }
 
 ##### MAIN #####
-group_level <- c('HexaPro','This study 1', 'This study 2', 'This study 3')
+group_level <- c('S-Closed/HexaPro','Non-fusogenic', 'Fusogenic', 'Variants')
 
 df <- read_tsv('result/S2_HR1_DMS_scores.tsv') %>%
-        mutate(grouping=mapply(coloring,mut)) %>%
+        mutate(grouping=factor(mapply(coloring,mut),levels=c('Others',group_level, 'WT'))) %>%
         filter(mut_class %in% c('missense', 'WT'))
 Model <- lm(df$fus_score~df$exp_score)
 df$residual <- Model$resid
 df_special <- filter(df, grouping %in% group_level)
 df_WT <- filter(df, grouping=='WT')
-df_others <- filter(df, grouping=='others')
-print (paste("correlatoin between fusion score and expression score: ", cor(df$exp_score, df$fus_score)))
+df_others <- filter(df, grouping=='Others')
+print (paste("correlation between fusion score and expression score: ", cor(df$exp_score, df$fus_score)))
 plot_exp_vs_fus(df, df_special, df_others, df_WT, 'graph/Exp_vs_fus.png', 'Fusion score')
 
 df <- mutate(df, fus_score=residual)
